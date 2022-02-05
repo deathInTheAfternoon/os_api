6d10c1937b97f216426677629fd17e1d3d19d746
Mr Motivator <naveen@example.com>
2022-02-05 16:11:42 +0000

    First commit

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+use nix::errno::Errno;
+use nix::unistd;
+use nix::sched;
+use std::ffi::CStr;
+use std::ffi::CString;
+use std::str::Utf8Error;
+use std::thread::current;
+
+fn main() {
+    let mut buf = [0u8; 64];
+    let hostname_cstr = unistd::gethostname(&mut buf).expect("Failed to get hostname");
+    let hostname = hostname_cstr.to_str().expect("Invalid hostname (not UTF-8)");
+    println!("Hostname {}", hostname);
+
+    let dir = unistd::getcwd().unwrap();
+    println!("Current working dir {:?}", dir);
+
+    let egid = unistd::getegid();
+    println!("Effective group id of this process: {}", egid);
+
+    let euid = unistd::geteuid();
+    println!("Effective user id of this process: {}", euid);
+
+    let user = CString::new("nthakur").unwrap();
+    let gid = unistd::Gid::from_raw(123);
+    let glist = unistd::getgrouplist(&user, gid);
+    println!("Group list for user {:?}", glist);
+
+    println!("Supplementary group IDs of calling process: {:?}", unistd::getgroups());
+
+    let pid = Some(unistd::Pid::this());
+    let pgid = unistd::getpgid(pid).expect("Failed to get pgid");
+    println!("Group id of calling proc with id {}: {}", pid.unwrap(), pgid);
+
+    println!("Group id of calling proc: {}", unistd::getpgrp());
+
+    println!("Parent pid: {}", unistd::getppid());
+
+    println!("Real, effective, saved group ids of this proc: {:?}", unistd::getresgid());
+
+    println!("Real, effective, saved user ids of this proc: {:?}", unistd::getresuid());
+
+    println!("This thread id: {}", unistd::gettid());
+
+    // Create UTS namespace
+    let current_hostname = get_host_name();
+    println!("Host hostname {}", current_hostname);
+
+    // How to respond to a specific error, in this case EPERM...
+    sched::unshare(sched::CloneFlags::CLONE_NEWUTS).unwrap_or_else(|error| {
+        if error == Errno::EPERM {
+            panic!("This process doesn't have permissions to call 'unshare()'");
+        } else {
+            panic!("Error {:?} calling unshare().", error);
+        }
+    });
+    println!("Successfully called unshare(CLONE_NEWUTS)");
+
+    let newhostname = "blahblah";
+    unistd::sethostname(newhostname).expect("sethostname() failed.");
+    println!("Succesfully changed hostname to {}", newhostname);
+
+    let result = get_host_name();
+    println!("Retreived hostname {}", result);
+}
+
+fn get_host_name() -> String {
+    let mut buf = [0u8; 64];
+    let hostname_cstr = unistd::gethostname(&mut buf).expect("Failed to get hostname.");
+    let iWantToReturnThis = hostname_cstr.to_str();
+    return iWantToReturnThis.ok().unwrap().to_owned();
+}
