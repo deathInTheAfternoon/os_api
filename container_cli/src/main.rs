31b951145b84fe4ba79247d69d52e6a9be09bac4
Mr Motivator <naveen@example.com>
2022-02-15 20:49:04 +0000

    Add more comments. Remove unused route.

--- container_cli/src/main.rs
+++ container_cli/src/main.rs
@@ -4,24 +4,24 @@ use clap::Parser;
 use rocket::State;
 
 // CLAP command line handling...
-/// The amazingly simple Rocket daemon
+/// Our amazingly simple Rocket daemon
 #[derive(Parser, Debug)]
 #[clap(name("Rocket daemon"), author("DeathInTheAfternoon"), version("0.1"), about("Does what is says on the tin."), long_about=None)]
 struct Args {
-    /// Set the listening port
+    /// Set the port on which we will listen
     /// 
     /// This parameter will configure the port on which this daemon listens for incoming requests.
     #[clap(short('p'), long("port"), default_value_t = 8080)]
     port: i32,
 
-    /// Set host IP to which we bind
+    /// Set bind IP on which we will listen
     /// 
-    /// When in Docker container bind to ALL interfaces 0.0.0.0 NOT 127.0.0.1.
+    /// When in container bind to ALL interfaces 0.0.0.0
     #[clap(short('a'), long("address"), default_value = "127.0.0.1")]
     address: String,
 }
 
-// Structure used to store state shared between handlers...
+// Structure used to store state shared between Rocket handlers...
 struct DaemonState {
     uuid: Uuid,
 }
@@ -31,19 +31,15 @@ fn index(hit_count: &State<DaemonState>) -> String {
     format!("My id is {}", hit_count.uuid)
 }
 
-#[get("/users")]
-fn get_users() -> &'static str {
-    "You're my first user"
-}
-
 #[rocket::main]
 async fn main() {
+    // Parse command line arguments using Clap
     let args = Args::parse(); 
-    // Start Rocket using a custom ip address and port number
+    // Start Rocket using the custom ip address and port number, if any
     let figment = rocket::Config::figment().merge(("port", args.port))
                                                     .merge(("address", args.address));
     rocket::custom(figment)
-            .mount("/", routes![index, get_users])
-            .manage(DaemonState { uuid: Uuid::new_v4(), })
+            .mount("/", routes![index])
+            .manage(DaemonState { uuid: Uuid::new_v4(), }) // Generate a uuid for this instance
             .launch().await;
 }
